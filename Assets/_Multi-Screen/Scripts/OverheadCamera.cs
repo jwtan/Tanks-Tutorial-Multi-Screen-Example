cfe6500e6cd7b2684cce3dd127784861c0443db5
Jian Wei Tan <jianwei.jwtan@gmail.com>
2020-02-07 18:01:42 +0800

    Multi-screen code commit

---
+++ Assets/_Multi-Screen/Scripts/OverheadCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class OverheadCamera : MonoBehaviour
+{
+    private Camera _camera;
+    private int _numDisplays;
+
+    private void Start()
+    {
+        _camera = GetComponent<Camera>();
+
+        UpdateDisplays();
+    }
+
+    private void Update()
+    {
+        if (_numDisplays == Display.displays.Length)
+        {
+            return;
+        }
+
+        UpdateDisplays();
+    }
+
+    private void UpdateDisplays()
+    {
+        _numDisplays = Display.displays.Length;
+
+        if (_numDisplays > 1)
+        {
+            Display target = Display.displays[1];
+            _camera.SetTargetBuffers(target.colorBuffer, target.depthBuffer);
+
+            _camera.enabled = true;
+        }
+        else
+        {
+            _camera.enabled = false;
+        }
+    }
+}
---
+++ Assets/_Multi-Screen/Scripts/TankCamera.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Complete
+{
+    public class TankCamera : MonoBehaviour
+    {
+        [SerializeField]
+        private Camera _camera = null;
+
+        public void Setup(int playerNumber)
+        {
+            if (playerNumber != 1)
+            {
+                Destroy(_camera.gameObject);
+            }
+            else
+            {
+#if !UNITY_EDITOR
+                Display target = Display.displays[0];
+                _camera.SetTargetBuffers(target.colorBuffer, target.depthBuffer);
+#endif
+            }
+        }
+    }
+}
