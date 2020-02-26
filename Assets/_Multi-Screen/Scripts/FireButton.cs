ff169232352f7df065fc84a84e4cfa2a66852617
Jian Wei Tan <jianwei.jwtan@gmail.com>
2020-02-26 16:07:42 +0800

    Added joystick controls

---
+++ Assets/_Multi-Screen/Scripts/FireButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Complete
+{
+    public class FireButton : MonoBehaviour
+    {
+        public string ButtonName = "Fire1";
+
+        private bool _pressed = false;
+
+        public void OnPointerDown()
+        {
+            _pressed = true;
+            Input.PressButtonDownMobile(ButtonName);
+        }
+
+        public void OnPointerUp()
+        {
+            _pressed = false;
+            Input.PressButtonUpMobile(ButtonName);
+        }
+
+        private void Update()
+        {
+            if (_pressed)
+                Input.PressButtonMobile(ButtonName);
+        }
+    }
+}
